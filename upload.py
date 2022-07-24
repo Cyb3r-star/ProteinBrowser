478a77e4dd5ea89f1a3b24853025f2799eab6df0
cyb3r-star <57905039+Cyb3r-star@users.noreply.github.com>
2022-07-24 16:06:54 -0400

    Added comments to the upload.py file

--- upload.py
+++ upload.py
@@ -2,6 +2,7 @@
 import xlrd
 import sqlite3 as sql
 
+#Prompting the user for input for Excel file, sheet, database, and some experiment data
 filename = input("Enter the name of the Excel file to parse: ")
 sheetname = input("Enter the name of the sheet to parse: ")
 database_name = input("Enter the name of the database to which you want to upload data: ")
@@ -9,9 +10,12 @@ exp_name = input("Enter the experiment name: ")
 ctrl = input("Enter the control name: ")
 mut = input("Enter the mutant name: ")
 conditions = input("Enter the experiment conditions: ")
+
+#Opening and loading the Excel sheet data using xlrd
 workbook = xlrd.open_workbook(("{}").format(filename))
 db = workbook.sheet_by_name(("{}").format(sheetname))
 
+#Declaring the Protein class to store individual data for a single protein (will be stored in the proteins[] list)
 class Protein:
     def __init__(self, accession, name, description, peptides, PSMs, ratios_raw, ratios_corrected, avg, N, SD, SEM, conf):
         self.accession = accession
@@ -30,7 +34,6 @@ class Protein:
 proteins = []
 
 #Iterating over the spreadsheet rows, extracting individual cell data, and assigning it to variables in the Protein class
-
 for row_count in range(3, 62):
     accession = db.cell(row_count, 0).value
     name = db.cell(row_count, 1).value
@@ -55,13 +58,10 @@ for row_count in range(3, 62):
 
 #Instantiating a new protein object with the attributes extracted from spreadsheet row
     new_protein = Protein(accession, name, description, peptides, PSMs, ratios_raw, ratios_corrected, avg, N, SD, SEM, conf)
-
     proteins.append(new_protein)
-#print(new_protein.accession, new_protein.name, new_protein.description, new_protein.peptides, new_protein.PSMs, new_protein.ratios_raw, "\n")
-
     row_count += 1
 
-#Exporting spreadsheet data to a SQLite database
+#Uploading spreadsheet data to a SQLite database
 conn = sql.connect("{}.db".format(database_name))
 curr = conn.cursor()
 
@@ -69,6 +69,7 @@ for i in proteins:
     add_protein_list = f"""INSERT INTO protein_list VALUES('{i.accession}', '{i.name}', '{i.description}')"""
     add_ratio_list = f"""INSERT INTO ratio_list VALUES('{i.accession}', '{exp_name}', '{i.avg}')"""
     add_exp_list = f"""INSERT INTO exp_list VALUES('{exp_name}', '{ctrl}', '{mut}', '{conditions}')"""
+
     print(add_protein_list,"\n", add_ratio_list,"\n", add_exp_list,"\n")
     curr.execute(add_protein_list)
     curr.execute(add_ratio_list)
